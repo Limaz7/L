3cc7916015317622ee87d0d6e88f96d44d2568f9
Lázaro <lazaro.2022315968@aluno.iffar.edu.br>
2024-08-16 09:31:00 -0300

    colocando card no materalize

--- materalize/index.php
+++ materalize/index.php
@@ -10,43 +10,159 @@
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+    <style>
+
+        .card{
+            transition: transform 0.3;
+        }
+
+        .card:hover {
+            transform: scale(1.1);
+            z-index: 1000;
+        }
+    </style>
     <title>Inicio</title>
 </head>
 
 <body>
     <?php include "headers.php" ?>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et totam adipisci dolore! Exercitationem officiis, illo nobis nam repudiandae, delectus reiciendis totam sequi obcaecati aut id, est magnam porro. Aperiam, culpa?
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et totam adipisci dolore! Exercitationem officiis, illo nobis nam repudiandae, delectus reiciendis totam sequi obcaecati aut id, est magnam porro. Aperiam, culpa?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum reiciendis dolorum autem magni ipsam veritatis id totam voluptatem provident esse expedita eaque doloribus reprehenderit ab, quod quos! Beatae, blanditiis placeat!
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et totam adipisci dolore! Exercitationem officiis, illo nobis nam repudiandae, delectus reiciendis totam sequi obcaecati aut id, est magnam porro. Aperiam, culpa?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum reiciendis dolorum autem magni ipsam veritatis id totam voluptatem provident esse expedita eaque doloribus reprehenderit ab, quod quos! Beatae, blanditiis placeat!
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et totam adipisci dolore! Exercitationem officiis, illo nobis nam repudiandae, delectus reiciendis totam sequi obcaecati aut id, est magnam porro. Aperiam, culpa?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum reiciendis dolorum autem magni ipsam veritatis id totam voluptatem provident esse expedita eaque doloribus reprehenderit ab, quod quos! Beatae, blanditiis placeat!
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et totam adipisci dolore! Exercitationem officiis, illo nobis nam repudiandae, delectus reiciendis totam sequi obcaecati aut id, est magnam porro. Aperiam, culpa?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum reiciendis dolorum autem magni ipsam veritatis id totam voluptatem provident esse expedita eaque doloribus reprehenderit ab, quod quos! Beatae, blanditiis placeat!
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et totam adipisci dolore! Exercitationem officiis, illo nobis nam repudiandae, delectus reiciendis totam sequi obcaecati aut id, est magnam porro. Aperiam, culpa?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum reiciendis dolorum autem magni ipsam veritatis id totam voluptatem provident esse expedita eaque doloribus reprehenderit ab, quod quos! Beatae, blanditiis placeat!
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et totam adipisci dolore! Exercitationem officiis, illo nobis nam repudiandae, delectus reiciendis totam sequi obcaecati aut id, est magnam porro. Aperiam, culpa?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum reiciendis dolorum autem magni ipsam veritatis id totam voluptatem provident esse expedita eaque doloribus reprehenderit ab, quod quos! Beatae, blanditiis placeat!
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum reiciendis dolorum autem magni ipsam veritatis id totam voluptatem provident esse expedita eaque doloribus reprehenderit ab, quod quos! Beatae, blanditiis placeat!
-    <div style="margin-top: 10px;">
-        <div class="row">
-            <div class="col s12 m2 offset-m3 center-align"><img src="logo01.webp" alt=""></div>
-            <div class="col s12 m2 center-align"><img src="logo02.webp" alt=""></div>
-            <div class="col s12 m2 center-align"><img src="logo03.webp" alt=""></div>
-            <div class="col s12 m3 center-align"></div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col s4 offset-s2"><img class="responsive-img" src="img01.webp" alt=""></div>
-        <div class="col s4"><img class="responsive-img" src="img02.webp" alt=""></div>
-    </div>
+    <main class="container">
 
-    <div class="row">
-        <div class="col s4 offset-s2"><img class="responsive-img" src="img03.webp" alt=""></div>
-        <div class="col s4"><img class="responsive-img" src="img04.webp" alt=""></div>
-    </div>
-    <script type="text/javascript" src="js/materialize.min.js"></script>
+        <div class="row">
+
+
+            <div class="col s12 m3">
+                <div style="background-color: black; color:white;" class="card">
+                    <div class="card-image">
+                        <img src="imagens/thia.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m3">
+                <div style="background-color: black; color:white;" class="card">
+                    <div class="card-image">
+                        <img src="imagens/thia.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m3">
+                <div style="background-color: black; color:white;" class="card">
+                    <div class="card-image">
+                        <img src="imagens/thia.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m3">
+                <div style="background-color: black; color:white;" class="card">
+                    <div class="card-image">
+                        <img src="imagens/thia.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m3">
+                <div style="background-color: black; color:white;" class="card">
+                    <div class="card-image">
+                        <img src="imagens/thia.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m3">
+                <div style="background-color: black; color:white;" class="card">
+                    <div class="card-image">
+                        <img src="imagens/thia.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m3">
+                <div style="background-color: black; color:white;" class="card">
+                    <div class="card-image">
+                        <img src="imagens/thia.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m3">
+                <div style="background-color: black; color:white;" class="card">
+                    <div class="card-image">
+                        <img src="imagens/thia.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+            </main>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+    </main>
 </body>
 
 </html>
